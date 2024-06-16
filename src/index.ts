d62f1f96ef0b575d7cc9c5c737e75ea4cbe49f2a
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2024-06-16 23:09:28 +0900

    make server
    api 제작 성공 ㄹㅇ 한잔해

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 type RecommendationMap = {
